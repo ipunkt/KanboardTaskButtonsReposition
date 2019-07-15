f4e74bad771a89e73a2b30e9693e080e7e9d3a9e
andrei <av@ipunkt.biz>
2019-07-15 15:00:37 +0200

    initial files created

---
+++ Plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kanboard\Plugin\PlusButtonReposition;
+
+use Kanboard\Core\Plugin\Base;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+
+    }
+
+    public function onStartup()
+    {
+    }
+
+    public function getPluginName()
+    {
+        return 'PlusButtonReposition';
+    }
+
+    public function getPluginDescription()
+    {
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'ipunkt Business Solutions';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return '';
+    }
+}
