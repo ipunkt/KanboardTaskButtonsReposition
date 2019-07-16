93486c0ddcdfc280ce3b0c76139595d7e77fe3c9
andrei <av@ipunkt.biz>
2019-07-16 12:56:14 +0200

    combined attachments on one line; added demo screenshot.

--- Template/task_file/show.php
+++ Template/task_file/show.php
@@ -8,7 +8,6 @@
 
 <li class="action-list">
     <?= $this->modal->medium('file', t('Attach a document'), 'TaskFileController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-</li>
-<li class="action-list">
+    &nbsp
     <?= $this->modal->medium('camera', t('Add a screenshot'), 'TaskFileController', 'screenshot', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
 </li>
