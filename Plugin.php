d96f2a2744f1888d0accd69a9dd7c5d413dc107a
andrei <av@ipunkt.biz>
2019-07-16 13:07:03 +0200

    plugin renamed; yml changed.

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kanboard\Plugin\PlusButtonReposition;
+namespace Kanboard\Plugin\TaskButtonsReposition;
 
 use Kanboard\Core\Plugin\Base;
 
@@ -8,18 +8,18 @@ class Plugin extends Base
 {
     public function initialize()
     {
-        $this->template->setTemplateOverride('task/sidebar', 'PlusButtonReposition:task/sidebar');
-        $this->template->setTemplateOverride('task/description', 'PlusButtonReposition:task/description');
-        $this->template->setTemplateOverride('task_file/show', 'PlusButtonReposition:task_file/show');
-        $this->template->setTemplateOverride('task_external_link/show', 'PlusButtonReposition:task_external_link/show');
-        $this->template->setTemplateOverride('task_internal_link/show', 'PlusButtonReposition:task_internal_link/show');
-        $this->template->setTemplateOverride('subtask/show', 'PlusButtonReposition:subtask/show');
-        $this->hook->on("template:layout:css", array("template" => "plugins/PlusButtonReposition/Assets/skin.css"));
+        $this->template->setTemplateOverride('task/sidebar', 'TaskButtonsReposition:task/sidebar');
+        $this->template->setTemplateOverride('task/description', 'TaskButtonsReposition:task/description');
+        $this->template->setTemplateOverride('task_file/show', 'TaskButtonsReposition:task_file/show');
+        $this->template->setTemplateOverride('task_external_link/show', 'TaskButtonsReposition:task_external_link/show');
+        $this->template->setTemplateOverride('task_internal_link/show', 'TaskButtonsReposition:task_internal_link/show');
+        $this->template->setTemplateOverride('subtask/show', 'TaskButtonsReposition:subtask/show');
+        $this->hook->on("template:layout:css", array("template" => "plugins/TaskButtonsReposition/Assets/skin.css"));
     }
 
     public function getPluginName()
     {
-        return 'PlusButtonReposition';
+        return 'TaskButtonsReposition';
     }
 
     public function getPluginDescription()
